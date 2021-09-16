37be91a7116525667eecd056942f1241b589b318
dgahn <dgahn@si-analytics.ai>
2021-09-17 03:39:14 +0900

    feat: html에서 추출한 문자열을 정렬하는 기능 추가

--- src/main/java/me/dgahn/extractor/Extractor.java
+++ src/main/java/me/dgahn/extractor/Extractor.java
@@ -1,5 +1,11 @@
 package me.dgahn.extractor;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.lang.Math.max;
+
 public class Extractor {
     private FilterType filterType;
     private Html html;
@@ -10,7 +16,47 @@ public class Extractor {
     }
 
     public String run() {
-        return filterAlphabetAndNumber(filterTag());
+        return sort(filterAlphabetAndNumber(filterTag()));
+    }
+
+    private String sort(String content) {
+        List<Character> capitals = extractCharacters("[^A-Z]", content);
+        List<Character> smalls = extractCharacters("[^a-z]", content);
+        List<Character> numbers = extractCharacters("[^0-9]", content);
+        return combine(capitals, smalls, numbers);
+    }
+
+    private String combine(List<Character> capitals, List<Character> smalls, List<Character> numbers) {
+        StringBuilder builder = new StringBuilder();
+        List<Character> english = combine(capitals, smalls);
+        int max = max(english.size(), numbers.size());
+        for (int i = 0; i < max; i++) {
+            if(english.size() > i) {
+                builder.append(english.get(i));
+            }
+            if(numbers.size() > i) {
+                builder.append(numbers.get(i));
+            }
+        }
+        return builder.toString();
+    }
+
+    private List<Character> combine(List<Character> capitals, List<Character> smalls) {
+        Stream<Character> combine = Stream.of();
+        for (int i = 65; i <= 90; i++) {
+            final int capital = (char) i;
+            Stream<Character> capitalStream = capitals.stream().filter(c -> c == capital);
+            Stream<Character> smallStream = smalls.stream().filter(c -> Character.toUpperCase(c) == capital);
+            combine = Stream.concat(combine, Stream.concat(capitalStream, smallStream));
+        }
+        return combine.collect(Collectors.toList());
+    }
+
+    private List<Character> extractCharacters(String regex, String content) {
+        return content.replaceAll(regex, "").chars()
+                .sorted()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toList());
     }
 
     private String filterAlphabetAndNumber(String content) {
--- src/test/java/me/dgahn/extractor/ExtractorTest.java
+++ src/test/java/me/dgahn/extractor/ExtractorTest.java
@@ -14,31 +14,37 @@ class ExtractorTest {
 
     @Test
     void HTML으로부터_내용을_추출할_때_HTML에서_태그를_포함할_수_있다() throws HtmlCreationFailException {
-        String url = "http://company.wemakeprice.com/wmp/";
-        Html html = htmlFactory.create(url);
-        FilterType filterType = FilterType.NONE;
-        Extractor extractor = new Extractor(filterType, html);
-        Assertions.assertTrue(extractor.run().trim().contains("!doctype html"));
+        Extractor extractor = createNoneTypeExtractor(FilterType.NONE);
+        String actual = extractor.run();
+        Assertions.assertEquals(8647, actual.length());
     }
 
     @Test
     void HTML으로부터_내용을_추출할_때_HTML에서_태그를_미포함할_수_있다() throws HtmlCreationFailException {
-        String url = "http://company.wemakeprice.com/wmp/";
-        Html html = htmlFactory.create(url);
-        FilterType filterType = FilterType.TAG;
-        Extractor extractor = new Extractor(filterType, html);
+        Extractor extractor = createNoneTypeExtractor(FilterType.TAG);
         String actual = extractor.run().trim();
-        Assertions.assertFalse(actual.contains("!doctype html"));
+        Assertions.assertEquals(3116, actual.length());
     }
 
     @Test
     void HTML으로부터_추출한_내용은_영소문자와_숫자만_포함한다() throws HtmlCreationFailException {
+        Extractor extractor = createNoneTypeExtractor(FilterType.TAG);
+        String actual = extractor.run();
+        Assertions.assertFalse(actual.contains("위메프"));
+    }
+
+    @Test
+    void HTML으로부터_추출한_내용은_영대문자_숫자_영소문자_숫자순으로_정렬한다() throws HtmlCreationFailException {
+        Extractor extractor = createNoneTypeExtractor(FilterType.TAG);
+        String actual = extractor.run();
+        Assertions.assertTrue(actual.startsWith("A0A0A0A0A0A0A0A0a0a0a0a0a0a0a0"));
+    }
+
+    private Extractor createNoneTypeExtractor(FilterType none) throws HtmlCreationFailException {
         String url = "http://company.wemakeprice.com/wmp/";
         Html html = htmlFactory.create(url);
-        FilterType filterType = FilterType.TAG;
-        Extractor extractor = new Extractor(filterType, html);
-        String actual = extractor.run().trim();
-        Assertions.assertFalse(actual.contains("위메프"));
+        FilterType filterType = none;
+        return new Extractor(filterType, html);
     }
 
 }
