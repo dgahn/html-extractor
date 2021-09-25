f65827f617b76f22b80df1a3d4de038527e223cb
dgahn <dgahn@si-analytics.ai>
2021-09-25 16:00:09 +0900

    refactor: html content을 mocking으로 처리

--- src/test/java/me/dgahn/extractor/ExtractorTest.java
+++ src/test/java/me/dgahn/extractor/ExtractorTest.java
@@ -1,48 +1,50 @@
 package me.dgahn.extractor;
 
-import me.dgahn.extractor.exception.HtmlCreationFailException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootTest
 class ExtractorTest {
 
-    @Autowired
-    private HtmlFactory htmlFactory;
-
     @Test
-    void HTML으로부터_내용을_추출할_때_HTML에서_태그를_포함할_수_있다() throws HtmlCreationFailException {
+    void HTML으로부터_내용을_추출할_때_HTML에서_태그를_포함할_수_있다() throws URISyntaxException, IOException {
         Extractor extractor = createNoneTypeExtractor(FilterType.NONE);
         String actual = extractor.run();
         Assertions.assertEquals(8647, actual.length());
     }
 
     @Test
-    void HTML으로부터_내용을_추출할_때_HTML에서_태그를_미포함할_수_있다() throws HtmlCreationFailException {
+    void HTML으로부터_내용을_추출할_때_HTML에서_태그를_미포함할_수_있다() throws URISyntaxException, IOException {
         Extractor extractor = createNoneTypeExtractor(FilterType.TAG);
         String actual = extractor.run().trim();
         Assertions.assertEquals(3116, actual.length());
     }
 
     @Test
-    void HTML으로부터_추출한_내용은_영소문자와_숫자만_포함한다() throws HtmlCreationFailException {
+    void HTML으로부터_추출한_내용은_영소문자와_숫자만_포함한다() throws URISyntaxException, IOException {
         Extractor extractor = createNoneTypeExtractor(FilterType.TAG);
         String actual = extractor.run();
         Assertions.assertFalse(actual.contains("위메프"));
     }
 
     @Test
-    void HTML으로부터_추출한_내용은_영대문자_숫자_영소문자_숫자순으로_정렬한다() throws HtmlCreationFailException {
+    void HTML으로부터_추출한_내용은_영대문자_숫자_영소문자_숫자순으로_정렬한다() throws URISyntaxException, IOException {
         Extractor extractor = createNoneTypeExtractor(FilterType.TAG);
         String actual = extractor.run();
         Assertions.assertTrue(actual.startsWith("A0A0A0A0A0A0A0A0a0a0a0a0a0a0a0"));
     }
 
-    private Extractor createNoneTypeExtractor(FilterType none) throws HtmlCreationFailException {
-        String url = "http://company.wemakeprice.com/wmp/";
-        Html html = htmlFactory.create(url);
+    private Extractor createNoneTypeExtractor(FilterType none) throws URISyntaxException, IOException {
+        Path path = Paths.get(getClass().getClassLoader().getResource("wemakeprice.html").toURI());
+        String content = Files.readString(path);
+        Html html = new Html(content);
         FilterType filterType = none;
         return new Extractor(filterType, html);
     }
